415c3ab6a120514b6116dcfea5241e606b929a15
Ewelina <65061737+rondelek@users.noreply.github.com>
2022-04-12 15:03:27 +0200

    Update classes_warriors_battle.py

--- classes_warriors_battle.py
+++ classes_warriors_battle.py
@@ -2,10 +2,13 @@ class Warrior:
     def __init__(self):
         self.health = 50
         self.attack = 5
+        self.defense = 0
+        self.vampirism = 0
 
     def is_attacked(self, other):
         if other.is_alive:
             self.health -= other.attack
+            other.health += other.vampirism/100 * other.attack
     
     @property
     def is_alive(self):
@@ -16,16 +19,26 @@ class Knight(Warrior):
     def __init__(self):
         self.health = 50
         self.attack = 7
+        self.defense = 0
+        self.vampirism = 0
 
 class Defender(Warrior):
     def __init__(self):
         self.health = 60
         self.attack = 3
         self.defense = 2
+        self.vampirism = 0
 
     def is_attacked(self, other):
         if other.is_alive:
             self.health -= max(0, other.attack - self.defense)
+
+class Vampire(Warrior):
+    def __init__(self):
+        self.health = 40
+        self.attack = 4
+        self.defense = 0
+        self.vampirism = 50
 
 
 class Army:
@@ -36,13 +49,12 @@ class Army:
         for n in range(number):
             self.soldiers.append(unit())
         
-
     def is_dead(self):
         self.soldiers.pop()
 
 class Battle:
     
-    def fight(self, army_1, army_2):    
+    def fight(self, army_1, army_2):
 
         while len(army_1.soldiers) > 0:
             if fight(army_1.soldiers[-1], army_2.soldiers[-1]):
@@ -63,7 +75,7 @@ def fight(unit_1, unit_2):
 
 
 
-# checkio test 
+# checkio tests
 
 chuck = Warrior()
 bruce = Warrior()
@@ -74,6 +86,10 @@ bob = Defender()
 mike = Knight()
 rog = Warrior()
 lancelot = Defender()
+eric = Vampire()
+adam = Vampire()
+richard = Defender()
+ogre = Warrior()
 
 fight(chuck, bruce) == True
 fight(dave, carl) == False
@@ -85,18 +101,25 @@ fight(carl, mark) == False
 carl.is_alive == False
 fight(bob, mike) == False
 fight(lancelot, rog) == True
+fight(eric, richard) == False
+fight(ogre, adam) == True
 
 my_army = Army()
-my_army.add_units(Defender, 1)
-    
+my_army.add_units(Defender, 2)
+my_army.add_units(Vampire, 2)
+my_army.add_units(Warrior, 1)
+
 enemy_army = Army()
 enemy_army.add_units(Warrior, 2)
+enemy_army.add_units(Defender, 2)
+enemy_army.add_units(Vampire, 3)
 
 army_3 = Army()
 army_3.add_units(Warrior, 1)
-army_3.add_units(Defender, 1)
+army_3.add_units(Defender, 4)
 
 army_4 = Army()
+army_4.add_units(Vampire, 3)
 army_4.add_units(Warrior, 2)
 
 battle = Battle()
